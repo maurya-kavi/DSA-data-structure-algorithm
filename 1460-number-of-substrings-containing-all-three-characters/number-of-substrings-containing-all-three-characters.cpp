9eee770832b617fb9b6b1bff8a3301710676a346
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-14 11:11:34 +0530

    Time: 35 ms (15.77%) | Memory: 11.2 MB (31.31%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -2,21 +2,31 @@ class Solution {
 public:
     int numberOfSubstrings(string s) {
         int n=s.size();
-        bool a=false, b=false, c=false;
-        int ans=0;
-        for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
-                if(s[j]=='a') a=true;
-                if(s[j]=='b') b=true;
-                if(s[j]=='c') c=true;
-                if(a && b && c){
-                    int extra=(n-1)-j;
-                    ans+=(1+extra);
-                    break;
-                }
+        int l=0;
+        int res=0;
+        // bool a=false, b=false,c=false;
+        unordered_map<char,int>mp;
+        mp['a']=0;
+        mp['b']=0;
+        mp['c']=0;
+        for(int r=0; r<n; r++){
+            if(s[r]=='a' || s[r]=='b' || s[r]=='c')  mp[s[r]]++;
+            // if(s[r]=='a') a=true;
+            // else if(s[r]=='b') b=true;
+            // else if(s[r]=='c') c=true;
+
+            while(mp['a']>0 && mp['b']>0 && mp['c']>0){
+                int extra=(n-1)-r;
+                res+=(1+extra);
+                
+                if(s[l]=='a' || s[l]=='b' || s[l]=='c') mp[s[l]]--;
+                // if(s[l]=='a') a=false;
+                // else if( s[l]=='b') b=false;
+                // else if(s[l]=='c') c=false;
+
+                l++;
             }
-            a=false,b=false,c=false;
         }
-        return ans;
+        return res;
     }
 };
