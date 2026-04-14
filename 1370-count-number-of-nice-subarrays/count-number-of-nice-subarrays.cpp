779c918f54ad322e117989a92229919b094ee6b7
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-14 10:49:03 +0530

    Time: 3 ms (89.21%) | Memory: 71.4 MB (94.60%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -2,18 +2,23 @@ class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n=nums.size();
-         int cnt=0;
-        unordered_map<int,int>mp;
-        mp[0]=1; // means if we got the ssubarray with the odd number equal to k then what extra odd number required 0 , means it can be one of the valid subarray
-        int cntt=0; //universal count
-        for(int i=0; i<n; i++){
-            if(nums[i] & 1) cntt++;
-            
-            if(mp.find(cntt-k)!=mp.end()) {
-                cnt+=mp[cntt-k];
+        return cntAtMost(nums,k)-cntAtMost(nums,k-1);
+    }
+
+// no of subarray with less than or equaal to the k odd numbers 
+    int cntAtMost(vector<int>&nums,int k){
+        int n=nums.size();
+        int l=0, cntt=0;
+        int res=0;
+        if(k<0) return 0;
+        for(int r=0; r<n; r++){
+            if(nums[r]&1) cntt++;
+            while(cntt>k){
+                if(nums[l]&1) cntt--;
+                l++;
             }
-            mp[cntt]++;
+            res+=(r-l+1); 
         }
-        return cnt;
+        return res;
     }
 };
