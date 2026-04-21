a2c446329338a3617deb998465cd1c57cead9d21
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-21 23:34:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (39.26%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        return max_element(nums.begin(),nums.end())-nums.begin();
+        // return max_element(nums.begin(),nums.end())-nums.begin();
+        //in this question, peak element doesn't always mean to be maximum
+        // peak element is any element which is greater than their neig.
+        int n=nums.size();
+        int l=0, h=n-1;
+        while(l<h){
+            int mid=(l+h)/2;
+            // given cond: nums[i] != nums[i + 1] for all valid i
+            if(nums[mid]<nums[mid+1]) l=mid+1;
+            else h=mid;
+        }
+        return l;
     }
 };
