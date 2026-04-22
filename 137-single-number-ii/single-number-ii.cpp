ae1e3524127086af08eeb162fa55abe5f092db6b
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-22 12:26:23 +0530

    Time: 3 ms (31.98%) | Memory: 13.4 MB (64.33%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result=0;
+        for(int i=0; i<32;i++){
+            int mask=1<<i;
+            int sum=0;
+            for(int num:nums){
+                if((num & mask )!=0){
+                    sum++;
+                }
+            }
+            if(sum%3!=0){
+                result=result|mask;
+            }
+        }
+        return result;
+    }
+};
