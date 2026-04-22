71c6059436ba41e333b452bb1101f74cfcaa297a
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-22 10:14:04 +0530

    Time: 4 ms (28.79%) | Memory: 8 MB (94.22%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -4,13 +4,13 @@ public:
         int result=0;
 
         for(int i=0; i<32; i++){
-            result=result<<1; // make the rightmost digit zero
+            result=result<<1; //create space for the next bit
 
             int lastbit=n&1;
 
             result=result|lastbit;
 
-            n=n>>1; //shift to second rightmost bit
+            n=n>>1; //do thee right shift
         }
         return result;
     }
