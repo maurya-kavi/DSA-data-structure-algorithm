781be4d4ee879eb7357fc06336293dbfce99ce73
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-24 19:49:18 +0530

    Time: 6 ms (70.30%) | Memory: 13 MB (73.03%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int f(int i,int j, string &s,string &t,vector<vector<int>>&dp){
-        if(i<0) return j+1; // first word i exhausted so insert the remaing element of word2 in word 1
-        if(j<0) return i+1; // if the second word is exhausted so delete the remaining chars of word1
-
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]) {
-            return dp[i][j]= f(i-1,j-1,s,t,dp);
-        }
-        else {
-            // 1+ min(delete,insert,replace)
-            return dp[i][j]= 1+min(f(i-1,j,s,t,dp), min(f(i,j-1,s,t,dp),f(i-1,j-1,s,t,dp)));
-        }
-    }
     int minDistance(string word1, string word2) {
         int n=word1.size();
         int m=word2.size();
-        //memoization
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,word1,word2,dp);
+        // tabulation : reduces the auxiliary space : tc=n*m + (n+m) => n*m in tabulation
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        for(int j=0; j<=m; j++) dp[0][j]=j;
+        for(int i=0; i<=n; i++) dp[i][0]=i;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1[i-1]==word2[j-1]) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1], dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];
     }
 };
