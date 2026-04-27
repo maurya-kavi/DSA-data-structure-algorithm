ccb4d154c55206717f62f45fa5a5637ee9a6e48d
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-27 17:12:04 +0530

    Time: 4 ms (82.47%) | Memory: 55.9 MB (49.55%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -1,46 +1,56 @@
 class Twitter {
 public:
-    vector<pair<int,int>>feed;
-    // unordered_map<int,vector<int>>mp; // for postTwwet
-    unordered_map<int,set<int>>followunfollowmp;
-    Twitter() {
-        
-    }
+    int time = 0;
+    
+    unordered_map<int, vector<pair<int,int>>> tweets;
+    unordered_map<int, set<int>> followees;
+
+    Twitter() {}
     
     void postTweet(int userId, int tweetId) {
-        feed.push_back({userId,tweetId});
-        // mp[userId].push_back(tweetId);
+        tweets[userId].push_back({time++, tweetId});
     }
     
     vector<int> getNewsFeed(int userId) {
-        //user ka khud ka post or jisko follow kiya hai wo rahega
-        set<int>s=followunfollowmp[userId];
-        vector<int>ans;
-        int cnt=0;
-        int sz=feed.size();
-        for(int i=sz-1; i>=0; i--){
-            auto [f,se]=feed[i];
-            if(f==userId){
-                cnt++;
-                ans.push_back(se);
-                if(cnt==10) break;
-                continue;
+        vector<int> ans;
+        
+        // max heap: {time, tweetId, userId, index}
+        priority_queue<tuple<int,int,int,int>> pq;
+        
+        // include self
+        followees[userId].insert(userId);
+        
+        // push latest tweet of each user
+        for(int user : followees[userId]) {
+            if(tweets[user].size() > 0) {
+                int idx = tweets[user].size() - 1;
+                auto [t, id] = tweets[user][idx];
+                pq.push({t, id, user, idx});
             }
-            if(!s.count(f)) continue;
-            cnt++;
-            ans.push_back(se);
-            if(cnt==10) break;
         }
+        
+        while(!pq.empty() && ans.size() < 10) {
+            auto [t, id, user, idx] = pq.top();
+            pq.pop();
+            
+            ans.push_back(id);
+            
+            if(idx > 0) {
+                auto [nt, nid] = tweets[user][idx - 1];
+                pq.push({nt, nid, user, idx - 1});
+            }
+        }
+        
         return ans;
     }
     
     void follow(int followerId, int followeeId) {
-        if(followerId==followeeId) return;
-        followunfollowmp[followerId].insert(followeeId);
+        if(followerId == followeeId) return;
+        followees[followerId].insert(followeeId);
     }
     
     void unfollow(int followerId, int followeeId) {
-        followunfollowmp[followerId].erase(followeeId);
+        followees[followerId].erase(followeeId);
     }
 };
 
