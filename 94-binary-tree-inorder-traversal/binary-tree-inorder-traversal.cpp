b9451fbf34b31a9c5dd95be2f8b7994644fa0368
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-28 13:14:27 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (10.12%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,18 +11,35 @@
  */
 class Solution {
 public:
-void dfs(TreeNode*root, vector<int>&ans){
-    if(root==nullptr){
-        return;
-    }
+// void dfs(TreeNode*root, vector<int>&ans){
+//     if(root==nullptr){
+//         return;
+//     }
 
-    dfs(root->left,ans);
-    ans.push_back(root->val);
-    dfs(root->right,ans);
-}
+//     dfs(root->left,ans);
+//     ans.push_back(root->val);
+//     dfs(root->right,ans);
+// }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int>ans;
-        dfs(root,ans);
+        if(!root) return ans;
+        unordered_map<TreeNode*,bool>mp;
+        stack<TreeNode*>s;
+        // if(root->right) s.push(root->right);
+        if(root) s.push(root);
+        // if(root->left) s.push(root->left);
+        while(!s.empty()){
+            TreeNode*node=s.top();
+            s.pop();
+            if(mp[node]==true) {
+                ans.push_back(node->val);
+                continue;
+            }
+            if(node->right) s.push(node->right);
+            s.push(node);
+            if(node->left) s.push(node->left);
+            mp[node]=true;
+        }
         return ans;
     }
 };
