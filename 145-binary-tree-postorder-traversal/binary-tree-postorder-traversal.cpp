fad445669e46df5befa1c0c60d51a377ab89c4c7
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-28 13:18:57 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (16.01%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -11,15 +11,27 @@
  */
 class Solution {
 public:
-void dfs(TreeNode*root,vector<int>&ans){
-    if(root==nullptr) return;
-    dfs(root->left,ans);
-    dfs(root->right,ans);
-    ans.push_back(root->val);
-}
+// void dfs(TreeNode*root,vector<int>&ans){
+//     if(root==nullptr) return;
+//     dfs(root->left,ans);
+//     dfs(root->right,ans);
+//     ans.push_back(root->val);
+// }
     vector<int> postorderTraversal(TreeNode* root) {
+        //post order LRN, we will build NRL then reverse it
+        //using one stack 
+        stack<TreeNode*>s;
         vector<int>ans;
-        dfs(root,ans);
+        if(!root) return ans;
+        s.push(root);
+        while(!s.empty()){
+            TreeNode*node=s.top();
+            s.pop();
+            ans.push_back(node->val);
+            if(node->left) s.push(node->left);
+            if(node->right) s.push(node->right);
+        }
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
