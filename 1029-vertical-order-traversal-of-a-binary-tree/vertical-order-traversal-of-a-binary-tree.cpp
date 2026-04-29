e08a8e5a9cd5adeb84524076baa353e428bb96a6
Kavindra Kumar <kavindramehta60@gmail.com>
2026-04-29 12:16:01 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (72.60%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        map<int,map<int,multiset<int>>>mp; // col,row,multiset
+        // first map->col will be in the sorteed order
+        // second map-> inside col, rows will be in the sorted order so top to bottom
+        // multiset has been used so we can have same value or different values in the sorted order for the same row and col for multiple nodes
+
+        queue<tuple<TreeNode*,int,int>>q; // treenode,row,col
+        q.push({root,0,0});
+        while(!q.empty()){
+            auto [node,row,col]=q.front();
+            q.pop();
+            mp[col][row].insert(node->val);
+            if(node->left) q.push({node->left,row+1,col-1});
+            if(node->right) q.push({node->right,row+1,col+1});
+
+        }
+
+        vector<vector<int>>result;
+        for(auto &p:mp){
+            //p.first = col
+            //p.second = map<int,multiset<int>>
+            vector<int>colNodes; 
+            // colNodes mein map<int,multiset<int>> ye saare element daal do
+            for(auto &q:p.second){
+                //q.first = row
+                //q.second= multiset<int>
+                for(int x:q.second){
+                    colNodes.push_back(x);
+                }
+            }
+            result.push_back(colNodes);
+        }
+        return result;
+    }
+};
