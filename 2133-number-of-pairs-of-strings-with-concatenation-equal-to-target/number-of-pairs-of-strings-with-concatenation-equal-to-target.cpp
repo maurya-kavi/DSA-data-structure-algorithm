1eb7108420cf9846bc71381d3119579912e6ee53
Kavindra Kumar <kavindramehta60@gmail.com>
2026-05-26 12:15:15 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (94.51%) - LeetSync

--- 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2133-number-of-pairs-of-strings-with-concatenation-equal-to-target/number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
+// lets optimize itttt
     int numOfPairs(vector<string>& nums, string target) {
         int n=nums.size();
         int cnt=0;
+        unordered_map<string,int>mp;
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                string t="";
-                t+=nums[i];
-                t+=nums[j];
-                if(t==target) cnt++;
-                string p="";
-                p+=nums[j];
-                p+=nums[i];
-                if(p==target) cnt++;
+            mp[nums[i]]++;
+        }
+        
+        for(auto&s:nums){
+            //check if s is the prefix of the target
+            if(target.find(s)==0){
+                //if s is prefix of target is then is is true
+                // now look for the rem is present in the map or not
+                string rem=target.substr(s.size()); // giving the begining index to get rem fromthere to last char of string target
+                cnt+=mp[rem];
+
+                //if prefix == sufix then cnt--;  as  i!=j
+                if(s==rem) cnt--;
             }
         }
         return cnt;
