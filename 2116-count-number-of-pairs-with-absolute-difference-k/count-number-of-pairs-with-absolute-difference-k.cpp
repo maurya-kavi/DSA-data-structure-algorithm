b743fbd6e3df3f55f47fd92139f3be0a7b4ddade
Kavindra Kumar <kavindramehta60@gmail.com>
2026-05-26 11:50:28 +0530

    Time: 4 ms (38.55%) | Memory: 17.1 MB (28.14%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.cpp
@@ -7,16 +7,9 @@ public:
         unordered_map<int,int>mp;
         for(int i=0; i<n; i++) mp[nums[i]]++;
         for(int i=0; i<(n-1); i++){
-            int l=i+1, r=n-1;
-            while(l<=r){
-                int mid=(l+r)/2;
-                if(nums[mid]-nums[i] == k) {
-                    int x=mp[nums[mid]];
-                    cnt+=x;
-                    break;
-                }
-                else if(nums[mid]-nums[i] > k) r=mid-1;
-                else l=mid+1;
+            int need=k+nums[i];
+            if(mp.find(need)!=mp.end()){
+                cnt+=mp[need];
             }
         }
         return cnt;
