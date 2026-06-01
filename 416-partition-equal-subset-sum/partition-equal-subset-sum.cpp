25e80425c18a874a554587bf6765e4b282492019
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-01 07:44:04 +0530

    Time: 57 ms (89.60%) | Memory: 13.5 MB (84.20%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int totSum=accumulate(nums.begin(), nums.end(), 0LL);
         int target=totSum/2;
         if(totSum%2!=0) return false;
 
-        dp.assign(n,vector<int>(target+1,-1));
         // now search an subset whose sum is equal to the target
-        return (rec(nums,target,0,n));
-    }
-
-    bool rec(vector<int>&nums, int target, int i, int n){
-        if(target==0) return true;
-        if(i==n) return false;
-        if(dp[i][target]!=-1) return dp[i][target];
-        // if notaken
-        bool notaken=rec(nums, target, i+1,n);
-        //if taken
-        bool taken=false;
-        if(target>=nums[i]) taken=rec(nums, target-nums[i], i+1, n);
-
-        return dp[i][target]=notaken || taken;
+        vector<bool>dp(target+1,false);
+        dp[0]=true;
+        for(int num:nums){
+            for(int i=target; i>=num; i--){
+                if(dp[i-num]==true){
+                    dp[i]=true;
+                }
+            }
+        }
+        return dp[target];
     }
 };
