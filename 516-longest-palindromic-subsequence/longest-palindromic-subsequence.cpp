2a0b1d0a562cfbec88fd854eda2c1830b1e8dcaa
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-02 10:01:47 +0530

    Time: 51 ms (78.35%) | Memory: 75.9 MB (62.88%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -6,13 +6,12 @@ public:
         return cnt(s,0,s.size()-1,dp);
     }
     int cnt(string &s, int i, int j,vector<vector<int>>&dp){
-
         if(i>j) return 0;
+        if(i==j) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
 
         if(s[i]==s[j]) {
-            if(i!=j) return dp[i][j]=2+cnt(s,i+1,j-1,dp);
-            else return dp[i][j]=1+cnt(s,i+1,j-1,dp);
+            return dp[i][j]=2+cnt(s,i+1,j-1,dp);
         }
         else return dp[i][j]=max(cnt(s,i+1,j,dp),cnt(s,i,j-1,dp));
     }
