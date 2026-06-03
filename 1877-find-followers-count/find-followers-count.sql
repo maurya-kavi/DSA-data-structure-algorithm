3ed52b7d0afe34c368f8db9d449b6e73fed8cfdd
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-03 18:21:09 +0530

    Time: 551 ms (62.71%) | Memory: 0B (100.00%) - LeetSync

--- 1877-find-followers-count/find-followers-count.sql
+++ 1877-find-followers-count/find-followers-count.sql
@@ -5,11 +5,19 @@
 -- order by  user_id
 
 #with clause
-with newTable as (
+-- with newTable as (
+--     select user_id, 
+--     count(follower_id) as followers_count
+--     from followers
+--     group by user_id order by user_id asc
+-- )
+-- select user_id,followers_count
+-- from newTable
+
+# using window function
+select distinct user_id, followers_count 
+from (
     select user_id, 
-    count(follower_id) as followers_count
+    count(follower_id) over(partition by user_id) as followers_count
     from followers
-    group by user_id order by user_id asc
-)
-select user_id,followers_count
-from newTable
+) as subquery
