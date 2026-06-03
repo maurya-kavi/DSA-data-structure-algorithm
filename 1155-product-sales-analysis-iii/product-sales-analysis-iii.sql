12ec8375f8c1dffe4a2ec27ab9186b24243bc9cd
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-03 17:35:43 +0530

    Time: 707 ms (60.63%) | Memory: 0B (100.00%) - LeetSync

--- 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -1,15 +1,13 @@
 # Write your MySQL query statement below
 # by using window function , modern approach
 
-with RankedSales as(
+select product_id,first_year,quantity,price
+from (
     select 
     product_id,
     year as first_year,
     quantity,
     price,
     rank() over(partition by product_id order by year asc) as rnk
-    from sales
-)
-select product_id,first_year,quantity,price
-from RankedSales
-where rnk=1;
+    from sales s) x
+where x.rnk=1;
