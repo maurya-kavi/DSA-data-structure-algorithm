5ccae13d3876533d6930eb3f99e2f651feef74a0
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-03 18:02:43 +0530

    Time: 525 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
+++ 596-classes-with-at-least-5-students/classes-with-at-least-5-students.sql
@@ -2,11 +2,23 @@
 
 # with clause
 
-with ClassCount as(
-    select class, count(class) as cnt
-    from courses 
-    group by class
-)
+-- with ClassCount as(
+--     select class, count(class) as cnt
+--     from courses 
+--     group by class
+-- )
 
-select class  from ClassCount
-where cnt>=5;
+-- select class  from ClassCount
+-- where cnt>=5;
+
+
+# another approach
+#by using window function
+select distinct class
+from (
+    select 
+    class,
+    count(student) over(partition by class) as student_cnt
+    from courses
+) as subquery
+where student_cnt>=5;
