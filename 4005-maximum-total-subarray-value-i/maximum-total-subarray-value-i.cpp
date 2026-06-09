6255aa5ad9e2839964055f6e9e36e3c8d555ebff
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-09 11:03:19 +0530

    Time: 0 ms (100.00%) | Memory: 103.6 MB (96.89%) - LeetSync

---
+++ 4005-maximum-total-subarray-value-i/maximum-total-subarray-value-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int n=nums.size();
+        int mx=*max_element(nums.begin(),nums.end());
+        int mn=*min_element(nums.begin(),nums.end());
+
+        return 1LL*(mx-mn)*k;
+    }
+};
