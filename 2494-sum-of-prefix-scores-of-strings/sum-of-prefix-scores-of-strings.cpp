bc482ed612a0377da6110802725f263ca814978c
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-16 15:21:39 +0530

    Time: 1257 ms (5.07%) | Memory: 711.9 MB (56.99%) - LeetSync

--- 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     struct trieNode{
-        int freq;
-        trieNode*children[26];
+        unordered_map<int,pair<int,trieNode*>>mp;
     };
     trieNode*getNode(){
         trieNode*newNode=new trieNode();
-        newNode->freq=0;
-        for(int i=0; i<26; i++){
-            newNode->children[i]=NULL;
-        }
         return newNode;
     }
     void insert(trieNode*root, string&word){
         trieNode*crawler=root;
         for(auto &ch:word){
             int ind=ch-'a';
-            if(crawler->children[ind]==NULL){
-                crawler->children[ind]=getNode();
+            if(!crawler->mp.count(ind)){
+                crawler->mp[ind].second=getNode();
             }
-            crawler=crawler->children[ind];
-            crawler->freq++;
+            crawler->mp[ind].first++;
+            crawler=crawler->mp[ind].second;
         }
     }
     vector<int> sumPrefixScores(vector<string>& words) {
@@ -34,19 +29,16 @@ public:
 
         for(int i=0; i<n; i++){
             string word=words[i];
-            // string str="";
             trieNode*crawler=root;
             int totfreq=0;
             for(auto &ch:word){
-                // str+=ch;
-                // now search for the str and find its freq as the prefix
                 int ind=ch-'a';
-                if(crawler->children[ind]==NULL){
+                if(!crawler->mp.count(ind)){
                     break;
                 }
-                crawler=crawler->children[ind];
-                int fre=crawler->freq;
+                int fre=crawler->mp[ind].first;
                 totfreq+=fre;
+                crawler=crawler->mp[ind].second;
             }
             result[i]=totfreq;
         }
