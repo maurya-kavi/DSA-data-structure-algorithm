e4669750cb9270051a4993a2c088b0689c6db741
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-16 09:20:04 +0530

    Time: 12 ms (11.36%) | Memory: 39.9 MB (42.05%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string processStr(string s) {
+        int n=s.size();
+        string result="";
+        for(int i=0; i<n; i++){
+            if(isalpha(s[i])) result+=s[i];
+            else if(s[i]=='*') {
+                if(!result.empty()) result.pop_back();
+            }
+            else if(s[i]=='#'){
+                result+=result;
+            }
+            else if(s[i]=='%'){
+                reverse(result.begin(),result.end());
+            }
+        }
+        return result;
+    }
+};
