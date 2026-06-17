10466fb63f6be6db9c0da1fde9f23548f2759fbd
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-17 17:15:02 +0530

    Time: 2 ms (76.94%) | Memory: 15.6 MB (58.02%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,13 +1,38 @@
 class Solution {
 public:
+    // Helper function to build the LPS array
+    vector<int> computeLPS(const string& pattern) {
+        int m = pattern.length();
+        vector<int> lps(m, 0);
+        
+        int len = 0; // Length of the previous longest prefix suffix
+        int i = 1;
+
+        while (i < m) {
+            if (pattern[i] == pattern[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    // Fall back to the previous known prefix
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
     string shortestPalindrome(string s) {
         string rev=s;
         reverse(rev.begin(),rev.end());
         int n=s.size();
-        for(int i=0; i<n; i++){
-            if(s.compare(0,n-i,rev,i,n-i)==0) return rev.substr(0,i)+s;
-        }
-        return rev+s;
+        string temp=s+"-"+rev;
+        vector<int>lps=computeLPS(temp);
+        int matchedLen=lps[temp.size()-1];
+        return rev.substr(0,(n-matchedLen))+s;
     }
 };
 
