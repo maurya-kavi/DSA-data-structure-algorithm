082d9855162f43ab5c2c70529ac71749a88b0270
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-23 17:46:32 +0530

    Time: 4 ms (61.23%) | Memory: 11.2 MB (84.73%) - LeetSync

---
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int> ugly(n);
+
+        ugly[0] = 1;
+
+        int i2 = 0;
+        int i3 = 0;
+        int i5 = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            int nextUgly = min({ugly[i2] * 2, ugly[i3] * 3, ugly[i5] * 5});
+
+            ugly[i] = nextUgly;
+
+            if (nextUgly == ugly[i2] * 2)
+                i2++;
+            if (nextUgly == ugly[i3] * 3)
+                i3++;
+            if (nextUgly == ugly[i5] * 5)
+                i5++;
+        }
+
+        return ugly[n - 1];
+    }
+};
