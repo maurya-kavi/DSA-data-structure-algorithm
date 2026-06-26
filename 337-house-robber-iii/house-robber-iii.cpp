05bc56923695d07832b77f3bf4de680ea440f28a
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-26 15:27:05 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (54.66%) - LeetSync

--- 337-house-robber-iii/house-robber-iii.cpp
+++ 337-house-robber-iii/house-robber-iii.cpp
@@ -12,56 +12,23 @@
 class Solution {
 public:
 
-unordered_map<TreeNode*, vector<int>> dp;
+    pair<int, int> solve(TreeNode* root) {
+        if (!root)
+            return {0, 0};
 
-void init(TreeNode* root){
-    if(!root) return;
-    dp[root] = {-1, -1};
-    init(root->left);
-    init(root->right);
-}
+        auto left = solve(root->left);
+        auto right = solve(root->right);
+
+        int rob = root->val + left.second + right.second;
+
+        int notRob = max(left.first, left.second)
+                   + max(right.first, right.second);
+
+        return {rob, notRob};
+    }
 
     int rob(TreeNode* root) {
-        init(root);
-        return solve(root,0);
+        auto ans = solve(root);
+        return max(ans.first, ans.second);
     }
-
-    int solve(TreeNode*root, int flag){
-
-        if(!root) return 0;
-
-        if (dp.count(root) && dp[root][flag] != -1)
-        return dp[root][flag];
-
-        int ans;
-
-        // root le lo
-        if(flag==0){
-            int r=root->val;
-            int left=0;
-            if(root->left) left=solve(root->left, 1);
-            int right=0;
-            if(root->right) right=solve(root->right, 1);
-
-            // if not taking root
-            int from_left=0;
-            if(root->left) from_left=solve(root->left,0);
-            int from_right=0;
-            if(root->right) from_right=solve(root->right,0);
-
-            ans= max(r+left+right, from_left+from_right);
-        }
-        else{
-            // cannot take the root due to flag =1
-            int from_left=0;
-            if(root->left) from_left=solve(root->left,0);
-            int from_right=0;
-            if(root->right) from_right=solve(root->right,0);
-
-            ans= (from_left+from_right);
-        }
-
-        return dp[root][flag] = ans;
-    }
-
 };
