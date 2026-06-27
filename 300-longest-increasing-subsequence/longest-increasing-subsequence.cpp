a60b42b86137c2674a64fd07a53aaa5406451fab
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-27 13:41:07 +0530

    Time: 63 ms (59.85%) | Memory: 14.2 MB (69.69%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,25 +1,40 @@
 class Solution {
 public:
-int dp[2505][2505];
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
         int n=nums.size();
-        return solve(0,-1,nums);
-    }
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        vector<int>dp(n+1,0);
 
-    int solve(int i, int prev, vector<int>&nums){
-        if(i==nums.size()) return 0;
-        
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        //take 
-        int take=0;
-        if(prev==-1 || nums[prev]<nums[i]){
-            take=1+solve(i+1, i,nums);
+        // lets say starting from ith and previous is at the 0th then this can be of atleast len =1
+
+        for(int i=1; i<=n; i++) dp[i]=1;
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<i; j++){
+                if(nums[i-1]>nums[j-1]) {
+                    dp[i]=max(dp[i], 1+dp[j]);
+                }
+            }
         }
-
-        // not take
-        int notake=solve(i+1,prev,nums);
-
-        return dp[i][prev+1]= max(take,notake);
+        return *max_element(dp.begin(),dp.end());
     }
+
+
+
+
+    // int solve(int i, int prev, vector<int>&nums){
+    //     if(i==nums.size()) return 0;
+
+    //     if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+    //     //take 
+    //     int take=0;
+    //     if(prev==-1 || nums[prev]<nums[i]){
+    //         take=1+solve(i+1, i,nums);
+    //     }
+
+    //     // not take
+    //     int notake=solve(i+1,prev,nums);
+
+    //     return dp[i][prev+1]= max(take,notake);
+    // }
 };
