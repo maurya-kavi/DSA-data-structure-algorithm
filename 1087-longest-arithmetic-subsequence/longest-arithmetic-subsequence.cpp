fd665c1b0b16d21817c6cdc003d4bd0938923642
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-27 17:59:55 +0530

    Time: 120 ms (79.84%) | Memory: 145.6 MB (71.88%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.cpp
@@ -1,34 +1,21 @@
 class Solution {
 public:
 
-// recursion + memo
-int dp[1001][1005];
+// tabulation
+
     int longestArithSeqLength(vector<int>& nums) {
         int n=nums.size();
-        memset(dp,-1,sizeof(dp));
-        int res=0;
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                int d=nums[j]-nums[i];
-                if(dp[i][d+500]!=-1) {
-                    res=max(res,dp[i][d+500]);
-                    continue;
-                }
-                res=max(res, 2+solve(i,d,nums));
+
+        vector<vector<int>>dp(n,vector<int>(1001,0));
+int ans=0;
+        for(int i=1; i<n; i++){
+            for(int j=0; j<i; j++){
+                int diff=nums[i]-nums[j];
+                dp[i][diff+500] = dp[j][diff+500] > 0 ? 1+dp[j][diff+500] : 2;
+                ans=max(ans, dp[i][diff+500]);
             }
         }
 
-        return res;
-    }
-
-    int solve(int i, int diff, vector<int>&nums){
-        if(i<0) return 0;
-        if(dp[i][diff+500]!=-1) return dp[i][diff+500];
-        int ans=0;
-        for(int j=i-1; j>=0; j--){
-            if(nums[i]-nums[j]==diff)  ans=max(ans, 1+solve(j,diff,nums));
-        }
-
-        return dp[i][diff+500] =ans;
+        return ans;
     }
 };
