03d4d4dab0805dc36ee7d6cbbfee6c279b6c7f6f
Kavindra Kumar <kavindramehta60@gmail.com>
2026-06-29 12:57:38 +0530

    Time: 91 ms (35.42%) | Memory: 75.9 MB (63.33%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
-    int longestPalindromeSubseq(string s) {
-        int n = s.size();
+    int lcs(string s, string t){
+        // tabulation approach
+        int n=s.size(), m=t.size();
 
-        vector<vector<int>> dp(n, vector<int>(n, 0));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
-        // Base case: single character
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = 1;
-        }
+        for(int j=0; j<=m; j++) dp[0][j]=0; // first row zero kardo
+        for(int i=0; i<=n; i++) dp[i][0]=0; // firt col zero kardo for the base case
 
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i + 1; j < n; j++) {
-
-                if (s[i] == s[j]) {
-                    dp[i][j] = 2 + dp[i + 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i + 1][j],
-                                   dp[i][j - 1]);
-                }
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1]==t[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j], dp[i][j-1]);
             }
-        }
 
-        return dp[0][n - 1];
+        }
+        return dp[n][m];
+    }
+    int longestPalindromeSubseq(string s) {
+        // brute force: generate all the subsequence and check if it is palindrome and store the length as the maximum one for the palindrome
+         //optimised: this can be solved by the similar approach as the lcs
+         // we will find the lcs of s and the reverse of s
+
+         string t=s;
+         reverse(t.begin(),t.end());
+         return lcs(s,t);
     }
 };
