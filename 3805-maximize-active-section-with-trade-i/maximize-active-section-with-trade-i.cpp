57acdcbd5e0a7bc1e3f7e52a50abc3bdcb02c300
Kavindra Kumar <kavindramehta60@gmail.com>
2026-07-22 11:00:05 +0530

    Time: 67 ms (56.25%) | Memory: 105.2 MB (28.13%) - LeetSync

--- 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
+++ 3805-maximize-active-section-with-trade-i/maximize-active-section-with-trade-i.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int n=s.size();
-        int ones=0;
-        for(int i=0;i<n;i++) if(s[i]=='1') ones++;
         string t="1"+s+"1";
-        int m=t.size();
-        vector<pair<char,int>> v;
-        for(int i=0;i<m;){
-            int j=i;
-            while(j<m&&t[j]==t[i]) j++;
-            v.push_back({t[i],j-i});
-            i=j;
-        }
-        int ans=ones;
-        for(int i=1;i+1<(int)v.size();i++){
-            if(v[i].first=='1' && v[i-1].first=='0' && v[i+1].first=='0'){
-                int gain=v[i-1].second+v[i+1].second;
-                ans=max(ans,ones+gain);
+        int n=t.size();
+        vector<int>cnt0;
+        int cnt=0;
+        int cnt1=0;
+        for(int i=0; i<n; i++){
+            if(t[i]=='0') cnt++;
+            else {
+                if(cnt>0) cnt0.push_back(cnt);
+                cnt=0;
+                cnt1++;
             }
         }
-        return ans;
+cnt1-=2;
+        int m=cnt0.size();
+        // if(m==1) return cnt1+cnt0[0]; // if only one block of zero then ans would be simply cnt1;
+        int mxpairsum=0;
+        for(int i=1; i<m; i++){
+mxpairsum=max(mxpairsum,cnt0[i]+cnt0[i-1]);
+        }
+
+        return mxpairsum+cnt1;
     }
 };
