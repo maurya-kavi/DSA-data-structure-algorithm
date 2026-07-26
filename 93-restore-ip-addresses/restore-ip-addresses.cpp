ba89e0fc9797dc4b48f88718effba8e0c5782712
Kavindra Kumar <kavindramehta60@gmail.com>
2026-07-26 20:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (20.03%) - LeetSync

--- 93-restore-ip-addresses/restore-ip-addresses.cpp
+++ 93-restore-ip-addresses/restore-ip-addresses.cpp
@@ -10,11 +10,12 @@ int n;
         return ans;
     }
 
-    void solve(string&s, int ind, int part, string curr){
+    // little modification trying do it using backtracking , its working either....
+    void solve(string&s, int ind, int part, string &curr){
         if(ind==n && part==4){
             if(curr.size()>0) curr.pop_back(); // remove the last . that had been put in the rec call
             ans.push_back(curr);
-            // curr+=".";
+            curr+=".";
             return ;
         }
         if(ind<n & part>=4) return;
@@ -23,29 +24,32 @@ int n;
         if(ind<n){
             int val=stoi(s.substr(ind,1));
             if(val<=255){
-                solve(s,ind+1,part+1,curr+s.substr(ind,1)+".");
-                // curr.pop_back();
-                // curr.pop_back();
+                curr+=s.substr(ind,1)+".";
+                solve(s,ind+1,part+1,curr);
+                curr.pop_back();
+                curr.pop_back();
             }
         }
         if(ind+1<n){
             int val=stoi(s.substr(ind,2));
             if(s[ind]!='0' && val<=255){
-                solve(s,ind+2,part+1,curr+s.substr(ind,2)+".");
-                                // curr.pop_back();
-                                // curr.pop_back();
-                                // curr.pop_back();
+                curr+=s.substr(ind,2)+".";
+                solve(s,ind+2,part+1,curr);
+                                curr.pop_back();
+                                curr.pop_back();
+                                curr.pop_back();
 
             }
         }
         if(ind+2<n){
             int val=stoi(s.substr(ind,3));
             if(s[ind]!='0' && val<=255){
-                solve(s,ind+3,part+1,curr+s.substr(ind,3)+".");
-                // curr.pop_back();
-                                // curr.pop_back();
-                                // curr.pop_back();
-                                // curr.pop_back();
+                curr+=s.substr(ind,3)+".";
+                solve(s,ind+3,part+1,curr);
+                curr.pop_back();
+                                curr.pop_back();
+                                curr.pop_back();
+                                curr.pop_back();
             }
         }
         
