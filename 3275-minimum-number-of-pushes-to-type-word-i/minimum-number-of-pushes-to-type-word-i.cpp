70188e4e28c6cd534721ff2a7ed34464e33e1539
Kavindra Kumar <kavindramehta60@gmail.com>
2026-07-31 11:47:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (54.24%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int minimumPushes(string word) {
+        // all letter are distinct 
         int n=word.size();
-        unordered_map<char,int>mp;
-        for(auto ch:word) mp[ch]++;
+        // unordered_map<char,int>mp;
+        // for(auto ch:word) mp[ch]++;
 
-        vector<int>fre;
-        for(auto [key,f]:mp) fre.push_back(f);
+        // vector<int>fre;
+        // for(auto [key,f]:mp) fre.push_back(f);
 
-        sort(fre.rbegin(), fre.rend());
+        // sort(fre.rbegin(), fre.rend());
         int cnt=0;
         int ans=0;
-        for(int i=0; i<fre.size(); i++){
+        for(int i=0; i<word.size(); i++){
             int x=cnt/8;
-            ans= ans+(x+1)*fre[i];
+            // ans= ans+(x+1)*fre[i];
+            ans= ans+(x+1);
             cnt++;
         }
         return ans;
