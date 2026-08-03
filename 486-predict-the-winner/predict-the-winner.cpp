9e52d4364a90cfba6821de5e990451fef636bd70
Kavindra Kumar <kavindramehta60@gmail.com>
2026-08-03 15:55:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (50.88%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -6,17 +6,17 @@ int dp[21][21];
         n = nums.size();
         memset(dp,-1,sizeof(dp));
         int p1=solve(nums,0,n-1);
-        int p2=accumulate(nums.begin(),nums.end(), 0LL)-p1;
+        // int p2=accumulate(nums.begin(),nums.end(), 0LL)-p1;
 
-        return p1>=p2;
+        return p1>=0;
     }
-
+// second approach for doing this problem using memoization , 
     int solve(vector<int>&nums, int i, int j){
         if(i==j) return nums[i];
         if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int takei=nums[i]+min(solve(nums,i+2,j), solve(nums, i+1,j-1));
-        int takej=nums[j]+min(solve(nums,i+1,j-1), solve(nums, i,j-2));
+        int takei=nums[i]-solve(nums,i+1,j);
+        int takej=nums[j]-solve(nums,i,j-1);
 
         return dp[i][j]= max(takei, takej);
     }
