ed4fde4cb3a2da128dd950ea5a7678ac50c6d121
Kavindra Kumar <kavindramehta60@gmail.com>
2026-08-03 13:29:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (95.73%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-
+int n;
+int dp[21][21];
     bool predictTheWinner(vector<int>& nums) {
-        int n = nums.size();
-        // dp[i][j] stores the maximum score difference for subarray nums[i...j]
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        
-        // Base case: subarrays of length 1
-        for (int i = 0; i < n; ++i) {
-            dp[i][i] = nums[i];
-        }
-        
-        // Build the DP table for subarrays of length 2 up to n
-        for (int length = 2; length <= n; ++length) {
-            for (int i = 0; i <= n - length; ++i) {
-                int j = i + length - 1;
-                
-                // Choose left vs choose right
-                int chooseLeft = nums[i] - dp[i + 1][j];
-                int chooseRight = nums[j] - dp[i][j - 1];
-                
-                dp[i][j] = max(chooseLeft, chooseRight);
-            }
-        }
-        
-        // Player 1 wins if their final score difference is 0 or more
-        return dp[0][n - 1] >= 0;
+        n = nums.size();
+        memset(dp,-1,sizeof(dp));
+        int p1=solve(nums,0,n-1);
+        int p2=accumulate(nums.begin(),nums.end(), 0LL)-p1;
+
+        return p1>=p2;
     }
+
+    int solve(vector<int>&nums, int i, int j){
+        if(i==j) return nums[i];
+        if(i>j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int takei=nums[i]+min(solve(nums,i+2,j), solve(nums, i+1,j-1));
+        int takej=nums[j]+min(solve(nums,i+1,j-1), solve(nums, i,j-2));
+
+        return dp[i][j]= max(takei, takej);
+    }
+    
 };
