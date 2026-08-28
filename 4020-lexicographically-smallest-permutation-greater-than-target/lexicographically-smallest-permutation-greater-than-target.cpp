76046125ae48e7904936f98b83d215645f715ff9
Kavindra Kumar <kavindramehta60@gmail.com>
2026-08-28 20:23:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (47.02%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,66 +1,49 @@
 class Solution {
 public:
-    string lexGreaterPermutation(string s, string target) {
-        unordered_map<char, int> mp;
-        for (char ch : s) mp[ch]++;
-
-        string prefix = "";
-        int n = s.size(), m = target.size();
-
-        // Greedily match target prefix as far as possible
+    string lexGreaterPermutation(string s, string t) {
+       int n = s.length(), m = t.length();
+        vector<int> cnt(26, 0);
+        for (char c : s) cnt[c - 'a']++;
+        
         int i = 0;
-        while (i < m && mp[target[i]] > 0) {
-            mp[target[i]]--;
-            prefix += target[i];
+        while (i < n && i < m && cnt[t[i] - 'a'] > 0) {
+            cnt[t[i] - 'a']--;
             i++;
         }
-
-        // Helper to append remaining available characters in sorted order
-        auto getSortedRemainder = [&](unordered_map<char, int>& freq) {
-            string rem = "";
-            for (char c = 'a'; c <= 'z'; c++) {
-                if (freq[c] > 0) {
-                    rem.append(freq[c], c);
+        
+        if (i < n && i < m) {
+            for (int c = t[i] - 'a' + 1; c < 26; c++) {
+                if (cnt[c] > 0) {
+                    string res = t.substr(0, i) + (char)('a' + c);
+                    cnt[c]--;
+                    for (int j = 0; j < 26; j++) res += string(cnt[j], 'a' + j);
+                    return res;
                 }
             }
-            return rem;
-        };
-
-        // If target is fully matched and s is longer, remaining chars form the answer
-        if (i == m && n > m) {
-            return prefix + getSortedRemainder(mp);
-        }
-
-        //  Backtrack right-to-left to find the first valid divergence point
-        for (int idx = i; idx >= 0; idx--) {
-            if (idx < m) {
-                char target_char = target[idx];
-                char best_c = '{';
-
-                // Find the smallest char strictly greater than target[idx]
-                for (auto [key, cnt] : mp) {
-                    if (cnt > 0 && key > target_char) {
-                        best_c = min(best_c, key);
-                    }
+        } else if (i == m && i < n) {
+            for (int c = 0; c < 26; c++) {
+                if (cnt[c] > 0) {
+                    string res = t.substr(0, i) + (char)('a' + c);
+                    cnt[c]--;
+                    for (int j = 0; j < 26; j++) res += string(cnt[j], 'a' + j);
+                    return res;
                 }
-
-                if (best_c != '{') {
-                    // Diverge here: place best_c and append sorted remaining chars
-                    mp[best_c]--;
-                    prefix.resize(idx); 
-                    prefix += best_c;
-                    return prefix + getSortedRemainder(mp);
-                }
-            }
-
-            // Undo match at idx - 1 and push character back to frequency map
-            if (idx > 0) {
-                char restored = prefix.back();
-                prefix.pop_back();
-                mp[restored]++;
             }
         }
-
+        
+        while (i > 0) {
+            i--;
+            cnt[t[i] - 'a']++;
+            for (int c = t[i] - 'a' + 1; c < 26; c++) {
+                if (cnt[c] > 0) {
+                    string res = t.substr(0, i) + (char)('a' + c);
+                    cnt[c]--;
+                    for (int j = 0; j < 26; j++) res += string(cnt[j], 'a' + j);
+                    return res;
+                }
+            }
+        }
+        
         return "";
     }
 };
