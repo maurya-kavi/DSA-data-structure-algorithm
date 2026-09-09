3757126b48e4d0188a9b444bc89d0bc837339e80
Kavindra Kumar <kavindramehta60@gmail.com>
2026-09-09 19:48:35 +0530

    Time: 13 ms (28.31%) | Memory: 61.5 MB (39.89%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
@@ -3,5 +3,24 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    return s.split(' ').map(word=>word.split('').reverse().join('')).join(' ');
+    let chars=s.split('');
+    let start=0;
+    for(let end=0; end<=chars.length; end++){
+        // when we reach a space or the end of the string , reverse the word 
+        if(end==chars.length || chars[end]==' '){
+            let left=start;
+            let right=end-1;
+
+            while(left<=right){
+                let temp=chars[left];
+                chars[left]=chars[right];
+                chars[right]=temp;
+                left++;
+                right--;
+            }
+            start=end+1;
+        }
+        // start=end+1;
+    }
+    return chars.join('');
 };
