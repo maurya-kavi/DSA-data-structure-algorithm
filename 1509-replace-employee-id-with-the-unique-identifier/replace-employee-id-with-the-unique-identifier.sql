e19ad821c0f835fb2045845969db63ff0a36dbba
Kavindra Kumar <kavindramehta60@gmail.com>
2026-09-11 16:08:34 +0530

    Time: 1415 ms (41.70%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,9 @@
-# Write your MySQL query statement below
-select employeeuni.unique_id, employees.name 
-from employees left join employeeuni
-on employeeuni.id = employees.id
+-- select * from employees as e 
+-- left join employeeuni as eu 
+-- on e.id=eu.id;
+
+-- ab jo jo specifically chahiye wo nikal lo
+
+select eu.unique_id , e.name from employees as e 
+left join employeeuni as eu 
+on e.id=eu.id;
