9ea0a17d1faf791b71cc6f27f38e1a749782c009
Kavindra Kumar <kavindramehta60@gmail.com>
2026-09-13 18:07:59 +0530

    Time: 256 ms (97.02%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,4 +1,22 @@
-select max(e.salary) as SecondHighestSalary
-from employee as e
-where e.salary<(select max(salary) from employee);
+-- select max(e.salary) as SecondHighestSalary
+-- from employee as e
+-- where e.salary<(select max(salary) from employee);
 
+
+-- select ifnull(
+--     (select distinct salary
+--     from employee
+--     order by salary desc
+--     limit 1 offset 1
+--     ),
+--     null
+-- ) as SecondHighestSalary
+
+
+with RankedSalaries AS(
+    select salary, dense_rank() over (order by salary desc) as rnk
+    from employee
+)
+select max(salary) as SecondHighestSalary
+from RankedSalaries
+where rnk=2;
