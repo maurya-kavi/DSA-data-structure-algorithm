9fa7f9baa71f83a98a25b51e229c544fc8e7834b
Kavindra Kumar <kavindramehta60@gmail.com>
2026-09-15 23:20:29 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (74.81%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -9,30 +9,86 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+
+ #include<bits/stdc++.h>
+using namespace std;
+
+// struct TreeNode {
+//   int val;
+//   TreeNode*left;
+//   TreeNode*right;
+//   TreeNode(int x){
+//       val=x;
+//       left=nullptr;
+//       right=nullptr;
+//   }
+// };
+
+
 class Solution {
-public:
-// void dfs(TreeNode*root,vector<int>&ans){
-//     if(root==nullptr) return;
-//     ans.push_back(root->val);
-//     dfs(root->left, ans);
-//     dfs(root->right,ans);
-// }
-    vector<int> preorderTraversal(TreeNode* root) {
-        //iterative approach for the preorder traversal of the binary tree
-        
-        vector<int>ans;
-        if(!root) return ans;
-        stack<TreeNode*>s;
-        if(root) s.push(root);
-        
-        // note: root is not moving , node is moving if you consider root then it will give you the mle
-        while(!s.empty()){
-            TreeNode*node=s.top();
-            ans.push_back(s.top()->val);
-            s.pop();
-            if(node->right) s.push(node->right);
-            if(node->left) s.push(node->left);
+    public:
+    
+    vector<int>preorderTraversal(TreeNode*root){
+        vector<int>pre;
+        TreeNode*cur=root;
+        while(cur){
+            if(cur->left==NULL){
+                pre.push_back(cur->val);
+                cur=cur->right;
+            }else{
+                
+                TreeNode*prev=cur->left;
+                while(prev->right && prev->right!=cur){
+                    prev=prev->right;
+                }
+                
+                if(prev->right==NULL){
+                    // make a thread connection
+                    pre.push_back(cur->val);
+                    
+                    prev->right=cur;
+                    
+                    cur=cur->left;
+                }else{
+                    // mtlb pahle se bana huaa tha toh connection hata do
+                    prev->right=NULL;
+                    cur=cur->right;
+                }
+            }
         }
-        return ans;
+        
+        return pre;
     }
 };
+
+// int main(){
+//     TreeNode*root=new TreeNode(1);
+//     root->left=new TreeNode(2);
+//     root->right=new TreeNode(3);
+//     root->left->left=new TreeNode(4);
+//     root->left->right=new TreeNode(5);
+//     root->left->right->right= new TreeNode(6);
+    
+//     Solution sol;
+//     vector<int>pre=sol.preorderTraversal(root);
+    
+//     cout<<"hello"<<endl;
+    
+//     for(int i=0; i<pre.size(); i++){
+//         cout<<pre[i]<<(i==pre.size()-1 ? "": " ");
+//     }
+//     cout<<endl;
+    
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
